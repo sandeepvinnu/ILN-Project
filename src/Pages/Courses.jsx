5960b44309138fdb269286959457576415a2697a
Sandeep Kumar <sandeepvinnu852@gmail.com>
2023-12-01 23:32:46 +0530

    Added some lines of code

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import UnderConstruction from '../Components/UnderConstruction/UnderConstruction'
+// import Corses from '../Components/Data/Courses.json'
 
 const Courses = () => {
   return (
+    
     <UnderConstruction />
   )
 }
