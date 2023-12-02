132afccb71c8ee3f89c717c33111a1c0a4dab122
Sandeep Kumar <sandeepvinnu852@gmail.com>
2023-12-03 00:18:11 +0530

    courses page

--- src/Components/BranchItem/BranchItem.jsx
+++ src/Components/BranchItem/BranchItem.jsx
@@ -3,8 +3,11 @@ import './BranchItem.css';
 import {Link} from 'react-router-dom';
 
 const BranchItem = ({ branchTitle, url }) => {
+  
+  const URL = {url}
+
   return (
-    <Link to={{ pathname: `/category/${url}/courses`, state: { data: {url} } }}>
+    <Link to={{ pathname: `/category/${url}/courses`, state: { data: URL } }}>
       <div className='branch-item'>
         <p>{branchTitle}</p>
       </div>
--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 // import UnderConstruction from '../Components/UnderConstruction/UnderConstruction'
 // import Corses from '../Components/Data/Courses.json'
 
-const Courses = ({location}) => {
+const Courses = () => {
   
+  const location = useLocation();
   const {state} = location;
+  // const {data} = state;
+  // const {URL} = data;
 
   return (
     <div>
       <h1>Page 2</h1>
-      <p>Data from Branches page: {state && state.data}</p>
+      <p>Data from Branches page: {state && state.data && state.data.url}</p>
     </div>
     // <UnderConstruction />
   )
